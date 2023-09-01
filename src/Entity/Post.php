7dae3ba9c06b27e728d1e53dc63e89f24bdc2117
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-09-01 17:36:40 -0600

    Filtrado

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
@@ -30,6 +33,12 @@ use Symfony\Component\Validator\Constraints\NotBlank;
     denormalizationContext: ['groups' => ['write']], // POST, PUT, PATCH
     paginationItemsPerPage: 8,
 )]
+#[ApiFilter(SearchFilter::class, properties: [
+    'title' => 'partial', // exact, partial, start, end, word_start
+    'body' => 'partial',
+    'category.name' => 'partial',
+])]
+#[ApiFilter(OrderFilter::class, properties: ['id'])]
 class Post
 {
     #[ORM\Id]
